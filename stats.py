2b0a25c43dce219ca1ccdf594217961f9558c0d0
exphixian <37256276+exphixian@users.noreply.github.com>
2018-03-22 15:33:16 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -4,30 +4,23 @@
 roll = input("Do you want your stats to be randomized?")
 roll = str.lower(roll)
 if roll == "yes":
-#    print("This is still underdevelopment.")
     while True:
         for i in range(6):
             print(random.randint(4,18))
         reroll = input("Do you want to keep these stats?")
         if reroll == "yes":
-            print("Please determine which variables will be used for each stat."
-                  "Class, race, and item variables will be added on later.")
-            strength = int(input("Enter your base strength:"))
-            constitution = int(input("Enter your base constitution:"))
-            dexterity = int(input("Enter your base dexterity:"))
-            intelligence = int(input("Enter your base intelligence:"))
-            wisdom = int(input("Enter your base wisdom:"))
-            charisma = int(input("Enter your base charisma:"))
+            print("Please determine which variables will be used for each stat.")
             break
 else:
-    print ("Please enter only base values."
-           "Class, race, and item variables will be added on later.")
-    strength = int(input("Enter your base strength:"))
-    constitution = int(input("Enter your base constitution:"))
-    dexterity = int(input("Enter your base dexterity:"))
-    intelligence = int(input("Enter your base intelligence:"))
-    wisdom = int(input("Enter your base wisdom:"))
-    charisma = int(input("Enter your base charisma:"))
+    print ("Please enter only base values.")
+
+print("Class, race, and item variables will be added on later.")
+strength = int(input("Enter your base strength:"))
+constitution = int(input("Enter your base constitution:"))
+dexterity = int(input("Enter your base dexterity:"))
+intelligence = int(input("Enter your base intelligence:"))
+wisdom = int(input("Enter your base wisdom:"))
+charisma = int(input("Enter your base charisma:"))
 
 stats = [strength, constitution, dexterity, intelligence, wisdom, charisma]
 print(" Str: %s\n Con: %s\n Dex: %s\n Int: %s\n Wis: %s\n Cha: %s\n" % tuple(stats))
