b2e3912b9518ceb31fa9381743f5abcf23a074cd
Kathleen Wilson <37256276+exphixian@users.noreply.github.com>
2018-05-06 16:46:34 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -2,11 +2,15 @@
 Character stats.
 - Right now, the stats are randomly generated between 4 and 18 (4 6-sided dice, dropping the lowest roll.)
 """
+import random
 
 def setstats():
-    global stats
-    stats = []
+    global strength, constitution, dexterity, intelligence, wisdom, charisma
     roll = str.lower(input("Do you want your stats to be randomized?\n"))
+    
+    #Change roll to no in order to remove random stat generation.
+    #roll = no
+    
     if roll == "yes":
         while True:
             for i in range(6):
@@ -18,11 +22,22 @@ def setstats():
     else:
         print("Please enter only base values.")
 
-    print("Class, race, and item variables will be added on later.")
-    for i in range(6):
-        stat = ['strength', 'constitution', 'dexterity', 'intelligence', 'wisdom', 'charisma']
-        stats.append(int(input("Enter your base %s\n" % (stat[i]))))
+    print("Class, race, and item variables will be added on later.\n")
+    
+    #The name of stats can be changed as needed.  Since this is D&D 3.5, we will be using the following.
+    strength = int(input("Enter your base strength:\n"))
+    constitution = int(input("Enter your base constitution:\n"))
+    dexterity = int(input("Enter your base dexterity:\n"))
+    intelligence = int(input("Enter your base intelligence:\n"))
+    wisdom = int(input("Enter your base wisdom:\n"))
+    charisma = int(input("Enter your base charisma:\n"))
     print()
-
+    
 setstats()
-print(stats)
+
+#If the stats are going to be adjusted later, define the stats list after to ensure accuracy...
+#Or just run this again I guess lol.
+stats = [strength, constitution, dexterity, intelligence, wisdom, charisma]
+
+#For troubleshooting
+#print(stats)
